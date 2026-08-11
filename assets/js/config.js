cc1fd5373c2a906773b97cdd882370559ae9b0ac
zovrotech <zovrotech@gmail.com>
2026-08-11 02:14:52 -0700

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,7 +0,0 @@
-/* Public Supabase client configuration.
-   This is the legacy anon key you supplied. Replace it later with the
-   project's publishable key when you rotate/migrate the project keys. */
-window.ZOVRO_CONFIG = {
-  supabaseUrl: 'https://cgwosspbrvqffmixvaza.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjd2dvc3NwYnJ2cWZmbWl4dmF6YSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg2NDM0MTYzLCJleHAiOjIxMDIwMTAxNjN9.VotRvLtnbtPy8-fQaQnHNzkjOHNAXN8I31HzjPMXXbc'
-};
